ac74ded1ffc7bec5837dde9ffdbfb9f917990358
wookim789 <wookim789@gmail.com>
2018-07-30 16:10:00 +0900

    add vo class

---
+++ jamPlan/src/main/java/com/spring/jamplan/model/UserVO.java
@@ -0,0 +1,19 @@
+package com.spring.jamplan.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserVO {
+	String id;
+	String email;
+	String pass;
+	String signDate;
+	String nation;
+	String gender;
+	String snsLink;
+	String travelType;
+	//image
+	int age;
+	boolean admin;
+	
+}
