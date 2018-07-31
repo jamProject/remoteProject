1cf3e20cdeb0977b24a411736c1b1ca593232054
wookim789 <wookim789@gmail.com>
2018-07-31 14:52:32 +0900

    change

---
+++ jamPlan/src/main/java/com/spring/jamplan/myroom/MyRoomDAOService.java
@@ -0,0 +1,8 @@
+package com.spring.jamplan.myroom;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyRoomDAOService implements MyRoomDAO{
+	
+}
--- jamPlan/src/main/java/com/spring/jamplan/myroom/MyRoomMapper.java
+++ jamPlan/src/main/java/com/spring/jamplan/myroom/MyRoomMapper.java
@@ -2,6 +2,9 @@ package com.spring.jamplan.myroom;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 import com.spring.jamplan.model.CalendarVO;
 import com.spring.jamplan.model.MessageVO;
 import com.spring.jamplan.model.PlanVO;
@@ -13,28 +16,11 @@ import com.spring.jamplan.model.UserVO;
  * @author Taehyuk, Kim
  *
  */
+@Mapper
+@Repository
 public interface MyRoomMapper {
 	// 팀리스트 보여주는 부분
-	ArrayList<TeamVO> getTeamList(UserVO user);
-	
-	// 팀 내 일정 보여주는 부분
-	ArrayList<PlanVO> getPlanList(UserVO user);
-	
-	// D-DAY를 계산하기 위해서 날짜 테이블에서 confirmedDate 컬럼을 가져온다.
-	String getConfirmedDate(CalendarVO calendar);
-	
-	// 내 정보 아이콘 눌렀을 경우
-	UserVO getUserInfo(String id);
-	
-	// 메시지함 눌렀을 경우
-	MessageVO getMessageList(String id);
-	
-	// 팀이름으로 검색한다.
-	TeamVO searchTeam();
-	int makeTeam();
-	
-	// 팀장만 일정을 추가할 수 있기 때문에 팀장인지 체크한 후, 일정 만들 수 있다.
-	int makePlan(UserVO user);
+
 	
 	
 	
