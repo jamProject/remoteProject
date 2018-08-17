ea511a45e4b3ddb18b742cca26dfcf98fb91fc82
SangHann <hshs1312@naver.com>
2018-08-17 21:17:28 +0900

    0817 project end

--- jamplan2/src/search.sql
+++ jamplan2/src/search.sql
@@ -1,55 +1,94 @@
-    CREATE TABLE PEOPLE (
-        ID VARCHAR2(20) PRIMARY KEY,
-        NAME VARCHAR2(10),
-        JOB VARCHAR2(20),
-        ADDRESS VARCHAR2(30),
-        BLOODTYPE VARCHAR2(3)
-    );
-    
-    select * from plan;
-    select * from people;
-    
-    drop table plan;
-    
-    create table plan (
+drop table plan;
+
+   create table plan (
         planNo number,
         teamNo number,
         goodCount number,
-        goodCheck number,
+        readCount number,
         image varchar2(2000),
         planName varchar2(2000),
         planDate varchar2(16)
         );
-  
+
+drop table plan;
+
+insert into plan values(1, 1, 10, 10, '이미지', '스위스여행', '20170801');
+insert into plan values(1, 1, 60, 100, '이미지', '베트남여행', '20180802');
+insert into plan values(1, 1, 70, 200, '이미지', '터키여행', '20150901');
+insert into plan values(1, 1, 40, 300, '이미지', '이탈리아여행', '20181201');
+insert into plan values(1, 1, 250, 400, '이미지', '영국여행', '20201220');
+insert into plan values(1, 1, 160, 500, '이미지', '미국여행', '20181225');
+insert into plan values(1, 1, 10, 10, '이미지', '스위스프링', '20170801');
+insert into plan values(1, 1, 10, 10, '이미지', '스가', '20170801');
+insert into plan values(1, 1, 10, 10, '이미지', '스나', '20170801');
+
+commit;
+
+CREATE TABLE imgTable (
+    img blob
+); 
+
+ENGINE=Innoblob DEFAULT CHARSET=utf8;
+
+select * from plan;
+
+select * from imgTable;
+
+drop table imgTable;
+
+
+
+select * from imgTable;
+
+SELECT img FROM imgTable;
+
+delete plan;
+
+commit;
+
+rollback;
+
+select * from board;
+
+select * from users;
+
+select * from sboard;
+
+select * from plan;
+
+desc plan;
+
+insert into plan(planNo, teamNo, goodCount, readCount, image, planName, planDate) values (1,2,3,4,'테스트','테스트',sysdate);
+
+
+commit;
+select * from plan order by planName;
+select * from plan where planName like '%스위스여행%';
+
+
+likeCheck
+select * from usertetst;
+
+select * from liketo;
+
+
+drop table liketo;
+
+create table likeTo(
+    planNo number,
+    userId varchar2(100)
+    );
     
-    insert into plan values (1, 1, 10, 58, '이미지', '스위스여행', '20161105');
-    insert into plan values (1, 1, 50, 100, '이미지', '스페인여행', '20161205');
-    insert into plan values (1, 1, 185, 6000, '이미지', '스웨덴', '20180805');
-    insert into plan values (1, 1, 110, 1000, '이미지', '한국여행', '20200805');
-    insert into plan values (1, 1, 2180, 10200, '이미지', '이탈리아여행', '20150805');
-    insert into plan values (1, 1, 580, 3720, '이미지', '프랑스여행', '20110805');
-    insert into plan values (1, 1, 580, 3720, '이미지', '터키여행', sysdate);
-    insert into plan values (1, 1, 580, 3720, '이미지', '프랑여행', sysdate);
-    insert into plan values (1, 1, 580, 3720, '이미지', '프스여행', sysdate);
-    
-    delete from plan;
-    
-    commit;
-    
-    CREATE TABLE USERINFO (
-   ID VARCHAR2(10) PRIMARY KEY, 
-   EMAIL VARCHAR2(20), 
-   PASS VARCHAR2(20), 
-   ISADMIN VARCHAR2(5), 
-   SIGNDATE DATE, 
-   NATION VARCHAR2(10), 
-   GENDER VARCHAR2(10), 
-   IMG VARCHAR2(100), 
-   AGE VARCHAR2(5), 
-   SNSLINK VARCHAR2(100), 
-   TRAVELTYPE VARCHAR2(10), 
-   HOBBY VARCHAR2(20) 
-   );
+insert into likeTo(userId) values ('admin');
+
+commit;
+
+select * from liketo;
+
+delete from liketo;
+
+
     
     
+ 
     
