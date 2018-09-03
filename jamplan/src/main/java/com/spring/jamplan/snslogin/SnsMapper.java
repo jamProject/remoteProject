51b7308624044e89e5a3899f090f11a6e3cf5e34
SangHann <hshs1312@naver.com>
2018-09-03 19:27:45 +0900

    0903project

--- jamplan/src/main/java/com/spring/jamplan/snslogin/SnsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring.jamplan.snslogin;
-
-import com.spring.jamplan.model.UserVO;
-
-public interface SnsMapper {
-	public void insertUser(UserVO vo);
-	public UserVO getUserInfo(String email);
-	
-}
